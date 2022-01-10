601961ef6d38908f71525694736671a55c833e4b
niyongwei <1500920628@qq.com>
2022-01-10 20:36:27 +0800

    1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import {callReactionFormDialog} from './views/reactionDialog'
 //导出处理表单弹窗的方法
-export const ReactionForm = ()=>{
-    callReactionFormDialog()
+export const ReactionForm = async()=>{
+    await callReactionFormDialog()
 }
--- src/views/reactionDialog.tsx
+++ src/views/reactionDialog.tsx
@@ -42,8 +42,6 @@ export const callReactionFormDialog = () => {
     ok(){
       console.log('ok')
     }
-  }).then((data) => {
-    console.log('>>>>>>>>>', data)
   })
 }
 
